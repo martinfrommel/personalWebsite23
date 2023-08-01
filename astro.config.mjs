c5ad89bdcb43013e6526f1e3255834a4bd593ab3
martinfrommel <martin.frommel@gmail.com>
2023-08-01 22:04:22 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config';
 
+import image from "@astrojs/image";
+import partytown from "@astrojs/partytown";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    image(),
+    // partytown(),
+    prefetch(),
+    sitemap(),
+    tailwind(),
+    alpinejs(),
+  ],
+  experimental: {
+    viewTransitions: true,
+  },
+});
