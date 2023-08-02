46e61a800b2207c7ab6719f69e9036f28e410619
martinfrommel <martin.frommel@gmail.com>
2023-08-02 13:15:56 +0200

    Dark Mode toggle adjusted to evenly change the mode, added persistence via localStorage, import AlpineJS via CDN to use plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import image from "@astrojs/image";
-import partytown from "@astrojs/partytown";
+// import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
+// import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
