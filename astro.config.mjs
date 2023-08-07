7339139f4ab5f67c253abb475ed52e5f1c64db21
martinfrommel <martin.frommel@gmail.com>
2023-08-07 10:44:54 +0200

    Changes to be committed: 	modified:   .gitignore 	modified:   astro.config.mjs 	deleted:    public/favicon.svg 	modified:   src/assets/bg/bgDoodles.svg 	new file:   src/components/Analytics.astro 	modified:   src/components/BaseHead.astro 	modified:   src/components/FeaturedLogos.astro 	modified:   src/components/FullScreenModal.astro 	modified:   src/components/MenuItems.astro 	modified:   src/pages/index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 import { defineConfig } from "astro/config";
-// import partytown from "@astrojs/partytown";
+import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    // partytown(),
-    prefetch(),
-    sitemap(),
-    tailwind(),
-  ],
+  integrations: [partytown(), prefetch(), sitemap(), tailwind()],
   experimental: {
     viewTransitions: true,
     assets: true,
