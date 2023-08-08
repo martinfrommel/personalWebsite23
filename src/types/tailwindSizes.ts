089410950dd6aeb12f0b352e4029141e1960862c
martinfrommel <martin.frommel@gmail.com>
2023-08-08 11:19:28 +0200

    Add TailwindSize types, Social Modal, Conditional rendering of Modal Heading/Footer on boolean prop

---
+++ src/types/tailwindSizes.ts
@@ -0,0 +1,26 @@
+export type TailwindSize =
+  | "0.5rem" // 2 in Tailwind
+  | "0.75rem" // 3 in Tailwind
+  | "1rem" // 4 in Tailwind
+  | "1.25rem" // 5 in Tailwind
+  | "1.5rem" // 6 in Tailwind
+  | "1.75rem" // 7 in Tailwind
+  | "2rem" // 8 in Tailwind
+  | "2.25rem" // 9 in Tailwind
+  | "2.5rem" // 10 in Tailwind
+  | "3rem" // 12 in Tailwind
+  | "3.5rem" // 14 in Tailwind
+  | "4rem" // 16 in Tailwind
+  | "5rem" // 20 in Tailwind
+  | "6rem" // 24 in Tailwind
+  | "8rem" // 32 in Tailwind
+  | "10rem" // 40 in Tailwind
+  | "12rem" // 48 in Tailwind
+  | "16rem" // 64 in Tailwind
+  | "20rem" // 80 in Tailwind
+  | "24rem" // 96 in Tailwind
+  | "32rem" // 128 in Tailwind
+  | "40rem" // 160 in Tailwind
+  | "48rem" // 192 in Tailwind
+  | "56rem" // 224 in Tailwind
+  | "64rem"; // 256 in Tailwind
